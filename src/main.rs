8634a21ec9b483ff5b255549b2ed6d601d8efeca
Luc Gerrits <luc.gerrits@univ-cotedazur.fr>
2023-10-13 01:08:04 +0200

    fix and better tests

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 use ethers::core::rand::thread_rng;
 use ethers::prelude::*;
+use eyre::Result;
 use serde::{Deserialize, Serialize};
 use std::convert::TryFrom;
-use std::ops::Add;
 use std::str::FromStr;
-use std::sync::Arc;
 use std::sync::atomic::{AtomicU64, Ordering};
+use std::sync::Arc;
 use std::{fs, time::Duration};
 use tokio::time::sleep;
-use eyre::Result;
 
 #[derive(Serialize, Deserialize, Clone)]
 struct KeyPair {
@@ -18,8 +17,21 @@ struct KeyPair {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // take the first argument as the number of txs per second
+    let args: Vec<String> = std::env::args().collect();
     // Parameters
-    let txs_per_second = 100;
+    let txs_per_second: u64 = args
+        .get(1)
+        .expect("Please provide the number of txs per second")
+        .parse::<u64>()
+        .expect("Please provide a valid number of txs per second");
+    //based on the TPS and benchmark duration, we can calculate the total number of txs
+    let benchmark_duration: u64 = args
+        .get(2)
+        .expect("Please provide the benchmark duration in seconds")
+        .parse::<u64>()
+        .expect("Please provide a valid benchmark duration in seconds");
+    let total_txs: u64 = txs_per_second * benchmark_duration;
     let sleep_duration = Duration::from_micros(1_000_000 / txs_per_second);
     let filename = "keypair.json";
     let key_pair: KeyPair;
@@ -52,7 +64,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let wallet = Wallet::from_bytes(&private_key_bytes)
         .expect("Failed to create wallet from private key bytes")
         .with_chain_id(42 as u64);
-    let mut nonce = provider
+    let nonce = provider
         .get_transaction_count(wallet.address(), None)
         .await
         .unwrap();
@@ -62,42 +74,28 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         Ok(id) => U64::from(id.as_u64()),
         Err(_) => U64::from(1),
     };
+    let start_block = provider_w_signer.get_block_number().await?;
+    let start_block_timestamp = provider_w_signer
+        .get_block(start_block)
+        .await?
+        .unwrap()
+        .timestamp;
+    // now timestamp
+    let start_time = chrono::Utc::now();
     println!("Connected to chain {}", chain_id);
     println!("Nonce: {}", nonce);
     println!("Address: {}", key_pair.address);
     println!("Sending {} transactions per second", txs_per_second);
-
-    // loop {
-    //     // Send a transaction (in this case, a simple ETH transfer)
-    //     let recipient = Address::from_str(&key_pair.address)?;
-    //     let tx = Eip1559TransactionRequest::new()
-    //         .nonce(nonce)
-    //         .to(recipient)
-    //         .max_priority_fee_per_gas(0u64)
-    //         .max_fee_per_gas(0u64)
-    //         .gas(21000u64)
-    //         .value(U256::exp10(18)); // Sending 100 wei
-    //     let pending_tx = provider_w_signer.send_transaction(tx, None).await?;
-    //     println!("Tx sent: 0x{:x}", pending_tx.tx_hash());
-
-    //     // get the mined tx
-    //     let _receipt = pending_tx
-    //         .await?
-    //         .ok_or_else(|| eyre::format_err!("tx dropped from mempool"))?;
-
-    //     // let tx = provider_w_signer.get_transaction(_receipt.transaction_hash).await?;
-    //     // println!("Sent tx: {}\n", serde_json::to_string(&tx)?);
-    //     // println!("Tx receipt: {}", serde_json::to_string(&_receipt)?);
-
-    //     nonce = nonce.add(1);
-    //     sleep(sleep_duration).await;
-    //     // break;
-    // }
-
+    println!("Benchmark duration: {} seconds", benchmark_duration);
+    println!("Total txs: {}", total_txs);
+    println!("Starting in 3 seconds...");
+    // wait a bit
+    sleep(Duration::from_secs(3)).await;
 
     let atomic_nonce = Arc::new(AtomicU64::new(nonce.as_u64()));
+    let processed_txs = Arc::new(AtomicU64::new(0));
 
-    loop {
+    while processed_txs.load(Ordering::SeqCst) < total_txs {
         let mut handles = Vec::with_capacity(txs_per_second as usize);
 
         for _ in 0..txs_per_second {
@@ -105,6 +103,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
             let provider_clone = provider_w_signer.clone();
             let key_pair_clone = key_pair.clone();
+            let processed_txs_clone = processed_txs.clone();
 
             let handle = tokio::spawn(async move {
                 let recipient = Address::from_str(&key_pair_clone.address).unwrap();
@@ -119,7 +118,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
                 let pending_tx = provider_clone.send_transaction(tx, None).await;
                 match pending_tx {
-                    Ok(tx) => println!("Tx sent: 0x{:x}", tx.tx_hash()),
+                    Ok(tx) => {
+                        println!("Tx sent: 0x{:x}", tx.tx_hash());
+                        processed_txs_clone.fetch_add(1, Ordering::SeqCst);
+                    }
                     Err(e) => println!("Error sending tx: {}", e),
                 }
             });
@@ -135,11 +137,28 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         sleep(sleep_duration).await;
     }
 
-
-
-
-
-
-    #[allow(unreachable_code)]
+    let end_block = provider_w_signer.get_block_number().await?;
+    let end_block_timestamp = provider_w_signer
+        .get_block(end_block)
+        .await?
+        .unwrap()
+        .timestamp;
+    let end_time = chrono::Utc::now();
+    println!("");
+    println!("");
+    println!("Benchmark finished");
+    println!("Start block: {}", start_block);
+    println!("End block: {}", end_block);
+    println!("Total txs: {}", total_txs);
+    println!("Expected TPS: {}", txs_per_second);
+    println!("Expected duration: {} seconds", benchmark_duration);
+    println!(
+        "Actual TPS: {:.2}",
+        (total_txs as f64) / (end_block_timestamp.as_u64() - start_block_timestamp.as_u64()) as f64
+    );
+    println!(
+        "Actual duration: {:.2} seconds",
+        (end_time - start_time).num_seconds()
+    );
     Ok(())
 }
